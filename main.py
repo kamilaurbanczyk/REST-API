05fb6277023e9d88e9b9552b71d4997c9a741cd9
kamilaurbanczyk <kamilaurbanczyk@interia.eu>
2021-02-26 18:32:51 +0100

    add Resource class

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'data': 'Hello World'}
+
+    def post(self):
+        return {'data': 'posted'}
+
+
+api.add_resource(HelloWorld, '/helloworld')
+
 if __name__ == '__main__':
     app.run(debug=True)
